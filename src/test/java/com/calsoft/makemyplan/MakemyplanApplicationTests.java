a218e1d7bc5c941250b3f26930079188e279e14d
Binthosh J <binthosh.kj@calsoftinc.com>
2021-02-22 13:50:58 +0530

    codeRefactor

---
+++ src/test/java/com/calsoft/makemyplan/MakemyplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calsoft.makemyplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemyplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
